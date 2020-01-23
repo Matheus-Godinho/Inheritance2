b6ce61781ce20bde439b101b31d73f46a71a896e
Matheus-Godinho <matheus.brc22@gmail.com>
2020-01-22 22:15:38 -0300

    Implemented entities

--- src/entities/Company.java
+++ src/entities/Company.java
@@ -1,5 +1,30 @@
 package entities;
 
-public class Company {
+public final class Company extends Taxpayer {
+
+	private Integer numberOfEmployees;
+	
+	public Company() {
+		super();
+	}
+	public Company(String name, Double annualIncome, Integer numberOfEmployees) {
+		super(name, annualIncome);
+		this.numberOfEmployees = numberOfEmployees;
+	}
+
+	public Integer getNumberOfEmployees() {
+		return numberOfEmployees;
+	}
+	public void setNumberOfEmployees(Integer numberOfEmployees) {
+		this.numberOfEmployees = numberOfEmployees;
+	}
+	
+	@Override
+	public Double tax() {
+		double taxRate;
+		
+		taxRate = (getNumberOfEmployees() > 10) ? 0.14 : 0.16;
+		return getAnnualIncome() * taxRate;
+	}
 
 }
--- src/entities/IndividualTaxpayer.java
+++ src/entities/IndividualTaxpayer.java
@@ -1,5 +1,30 @@
 package entities;
 
-public class IndividualTaxpayer {
+public final class IndividualTaxpayer extends Taxpayer {
+
+	private Double healthExpenditures;
+	
+	public IndividualTaxpayer() {
+		super();
+	}
+	public IndividualTaxpayer(String name, Double annualIncome, Double healthExpenditures) {
+		super(name, annualIncome);
+		this.healthExpenditures = healthExpenditures;
+	}
+
+	public Double getHealthExpenditures() {
+		return healthExpenditures;
+	}
+	public void setHealthExpenditures(Double healthExpenditures) {
+		this.healthExpenditures = healthExpenditures;
+	}
+	
+	@Override
+	public Double tax() {
+		double taxRate;
+		
+		taxRate = (getAnnualIncome() < 20000.00) ? 0.15 : 0.25;
+		return getAnnualIncome() * taxRate - getHealthExpenditures() * 0.50;
+	}
 
 }
