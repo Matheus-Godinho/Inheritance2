4cd3a772a3320a08daad107160ef3a27d01c574f
Matheus-Godinho <matheus.brc22@gmail.com>
2020-01-22 22:24:08 -0300

    Declared variables

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,25 @@
 package application;
 
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Taxpayer;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Taxpayer> taxpayers;
+		Taxpayer taxpayer;
+		String name;
+		double annualIncome;
+		char option;
+		int n;
+		
+		sc.close();
 
 	}
 
